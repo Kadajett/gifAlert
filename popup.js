53989581f92c4ff034e4e6c9558bd64375e029b0
Jeremy Stover(Kadajett) <jeremy.ryan.stover@gmail.com>
2015-11-19 14:48:08 -0800

    added zepto

--- popup.js
+++ popup.js
@@ -1,9 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+});
+
+window.app = (function(){
+  var firebaseRef = new Firebase("https://gifalert.firebaseio.com/");
+
+  firebaseRef.child("alerts").on("child_added", catchAlert);
+
+  function catchAlert(alert){
+    var value = alert.val();
+    console.log('Alert Caught: ', value);
+    routeAlert(value);
+  };
+
+  function routeAlert(alert){
+    switch (alert) {
+      case "letsplayminecraft":
+        gifs.execute.minecraft();
+        break;
+      case "leftShark":
+        gifs.execute.leftShark();
+        break;
+      default:
+        console.log("routeAlert: ", "Unable to find gif with id: " + alert);
+        break;
+    }
+  };
+
+  var gifs = (function(){
+
+    var api = {
+      execute: {
+        minecraft: executeMinecraft,
+        leftShark: executeLeftShark
+      }
+    };
+
+    function executeMinecraft(){
+      chrome.tabs.executeScript(null, {file: 'minecraft.js'});
+    };
+
+    function executeLeftShark(){
+      chrome.tabs.executeScript(null, {file: 'leftShark.js'});
+    };
+
+    var __api = this;
+    return api;
+  })();
+
   document.getElementById('minecraft').addEventListener("click", function() {
-    chrome.tabs.executeScript(null, {file: 'minecraft.js'});
+
   });
 
   document.getElementById('leftShark').addEventListener("click", function() {
-    chrome.tabs.executeScript(null, {file: 'leftShark.js'});
+
   });
-});
+
+  return {test: 'asdf'};
+})();
